ea20dba832584aaaca1792f445159e8efe12bea7
510004015 <105718141+510004015@users.noreply.github.com>
2022-09-20 04:19:11 +0800

    Update IP_API.js

--- IP_API.js
+++ IP_API.js
@@ -5,7 +5,7 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = ' ' + obj['city'] + ' ' + "IP:" + obj['query'];
+var subtitle = "♥" + obj['city'] + ' ' + "IP:" + obj['query'];
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "IP" + ":" + obj['query'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
 
