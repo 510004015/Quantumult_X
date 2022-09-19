adf1fd2fb9ada468476a9f080970e0671eea37d3
510004015 <105718141+510004015@users.noreply.github.com>
2022-09-20 03:35:50 +0800

    Update Test.js

--- Test.js
+++ Test.js
@@ -7,7 +7,7 @@ var obj = JSON.parse(body);
 var title = obj['country'];
 var subtitle = obj['city'] + ' ' + obj['query'] + ' ' + obj['timezone'];
 var ip = obj['query'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "IP" + ":" + obj['query'] + '\n' +'时区:'+ obj['timezone'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "IP" + ":" + obj['query'];
 
 
 $done({title, subtitle, ip, description});
