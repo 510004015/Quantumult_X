ff23d3428291ad00a34b64157dfef7960fa2fcf7
510004015 <105718141+510004015@users.noreply.github.com>
2022-09-20 03:07:27 +0800

    Update Test_IP.js

--- Test_IP.js
+++ Test_IP.js
@@ -5,6 +5,7 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
+var subtitle = obj['city'] + ' ' + obj['query'];
 
 
 $done({title});
