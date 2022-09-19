1f9817770f6ede23f66f0407a81970a99cd3379e
510004015 <105718141+510004015@users.noreply.github.com>
2022-09-20 03:28:54 +0800

    Update Test.js

--- Test.js
+++ Test.js
@@ -5,9 +5,9 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = obj['city'] + ' ' + obj['query'];
+var subtitle = obj['city'] + ' ' + obj['query'] + ' ' + obj['timezone'];
 var ip = obj['query'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "IP" + ":" + obj['query'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "IP" + ":" + obj['query'] + '\n' +'时区:'+ obj['timezone'];
 
 
 $done({title, subtitle, ip, description});
