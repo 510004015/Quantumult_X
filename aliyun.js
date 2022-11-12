5d41346f927e441af03b3e40afde248761148c70
510004015 <105718141+510004015@users.noreply.github.com>
2022-11-12 12:19:07 +0800

    Update aliyun.js

--- aliyun.js
+++ aliyun.js
@@ -5,7 +5,7 @@
 [rewrite_local]
 
 # ～ 阿里云盘（2022-10-16）
-^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body 
+^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/aliyun.js
 
 [mitm]
 
