37283c7e6a6ed099dcacbb28d594a679c6453cbe
510004015 <105718141+510004015@users.noreply.github.com>
2022-11-12 12:27:01 +0800

    Update aliyun.js

--- aliyun.js
+++ aliyun.js
@@ -1,17 +1,15 @@
-/***********************************
-
-
+/*
 
 [rewrite_local]
 
-# ～ 阿里云盘（2022-10-16）
+# > 阿里云盘优化
 ^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/aliyun.js
 
 [mitm]
 
 hostname=api.aliyundrive.com
 
-***********************************/
+*/
 
 let obj = JSON.parse($response.body);
 if (obj.result) {
