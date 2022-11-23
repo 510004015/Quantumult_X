4b6588d1f643d29f61249656c16e3d3c33147c70
510004015 <105718141+510004015@users.noreply.github.com>
2022-11-23 14:38:37 +0800

    Create zhbkp.js

---
+++ zhbkp.js
@@ -0,0 +1,16 @@
+/*
+
+[rewrite_local]
+
+# > 郑好办开屏广告
+^https:\/\/app-izz\.zhengzhou\.gov\.cn:10019\/bizgw\/gateway\.do url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/zhbkp.js
+
+[mitm]
+
+hostname = app-izz.zhengzhou.gov.cn
+
+*/
+
+let OBJ = JSON.parse($response.body);
+OBJ.data.data=[{}]
+$done({ body: JSON.stringify(OBJ) });
