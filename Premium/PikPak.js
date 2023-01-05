fe6ca14cf5ac37e28364eb7c5d539bcc39c6c5c6
510004015 <105718141+510004015@users.noreply.github.com>
2023-01-05 22:26:42 +0800

    Create PikPak.js

---
+++ Premium/PikPak.js
@@ -0,0 +1,18 @@
+/*
+
+[rewrite_local]
+
+^https:\/\/api-drive\.mypikpak\.com\/vip\/v1\/vip\/info url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/PikPak.js
+
+[mitm] 
+
+hostname = api-drive.mypikpak.com
+
+*/
+
+let Premium = $response.body;
+var modified = JSON.parse(Premium);
+modified.data.status = "ok",
+modified.data.expire = "2099-01-01T00:00:00+08:00",
+modified.data.type = "platinum";
+$done({body:JSON.stringify(modified)});
