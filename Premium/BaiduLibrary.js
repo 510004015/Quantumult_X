98436c1020e01d2abd88199d1dc352d254e16da2
510004015 <105718141+510004015@users.noreply.github.com>
2023-01-14 23:21:14 +0800

    Update BaiduLibrary.js

--- Premium/BaiduLibrary.js
+++ Premium/BaiduLibrary.js
@@ -12,7 +12,5 @@ hostname = appwk.baidu.com
 
 let Premium = $response.body;
 var modified = JSON.parse(Premium);
-
 modified.data.vip.base_vip_info = {"end_time":4070880000,"uid":510004015,"is_vip":1,"pro_total":0,"start_time":1672502400,"type":2,"remain_day":365,"normal_total":0};
-
 $done({body:JSON.stringify(modified)});
