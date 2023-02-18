6865e2c1153557a9fd52032acbbf5404c12eb119
510004015 <105718141+510004015@users.noreply.github.com>
2023-02-18 23:03:17 +0800

    Create mg.js

---
+++ Premium/mg.js
@@ -0,0 +1,19 @@
+/*
+
+[rewrite_local]
+
+^https:\/\/backend\.mealligram\.com\/api\/v\d\/purchase\/verifyReceipt url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/mg.js
+
+[mitm]
+
+hostname = backend.mealligram.com
+
+*/
+
+let Premium = $response.body
+var modified = JSON.parse(Premium);
+modified = {
+  "expiresAt" : 4070880000,
+  "productId" : "com.LeFal.Mealligram.pro.oneYear"
+};
+$done({body:JSON.stringify(modified)});
