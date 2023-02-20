91615a496813a2c7d0b6c88a25fc76f8215ef49e
510004015 <105718141+510004015@users.noreply.github.com>
2023-02-20 15:20:06 +0800

    Update Quark.js

--- Premium/Quark.js
+++ Premium/Quark.js
@@ -10,12 +10,12 @@ hostname = quark-api.uc.cn
 
 */
 
-let Premium = $response.body
+let Premium = $response.body;
 var modified = JSON.parse(Premium);
 modified.data = {
     "member_type" : "SUPER_VIP",
     "super_vip_exp_at" : 4070880000000,
     "total_capacity" : 6597069766656,
-    "use_capacity" : 0,
+    "use_capacity" : modified.data.use_capacity,
   };
 $done({body:JSON.stringify(modified)});
