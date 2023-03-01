088154df84587e4f3090268b2f64e43dc6654351
510004015 <105718141+510004015@users.noreply.github.com>
2023-03-02 00:11:54 +0800

    Update BaiduLibrary.js

--- JavaScript/BaiduLibrary.js
+++ JavaScript/BaiduLibrary.js
@@ -3,7 +3,7 @@ var modified = JSON.parse($response.body);
 const URL1 = 'messagetab';
 const URL2 = 'messagenewlist';
 if (url.indexOf(URL1) != -1) {
- modified.data.cardList.splice(1,1);
+ modified.data.cardList = [];
 };
 if (url.indexOf(URL2) != -1) {
  modified.data.msgList = [];
