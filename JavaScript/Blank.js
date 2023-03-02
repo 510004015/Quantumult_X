2ac6290647bca25893ba76c0a950b5f382f305fe
510004015 <105718141+510004015@users.noreply.github.com>
2023-03-03 00:49:01 +0800

    Update Blank.js

--- JavaScript/Blank.js
+++ JavaScript/Blank.js
@@ -1,4 +1,4 @@
-var Title = ' ';
-var Subtitle = 'Tunnel Opening';
-var Description = '暂不支持';
+var Title = "测试";
+var Subtitle = "Tunnel Opening";
+var Description = "暂不支持";
 $done({Title, Subtitle, Description});
