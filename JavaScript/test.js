115f52ad11d498cab805ffaf8d32d3a60fa1f6bd
510004015 <105718141+510004015@users.noreply.github.com>
2023-03-03 01:04:23 +0800

    Update and rename Simple.js to test.js

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,7 +1,7 @@
 if ($response.statusCode != 200) {$done(null)}
 var obj = JSON.parse($response.body);
-var Title = ' ';
-var Subtitle = 'Tunnel Opening';
+var title = ' ';
+var subtitle = 'Tunnel Opening';
 var ip = obj['query'];
 var description = "IP" + ":" + obj['query'] + '\n' + "国家或地区" + ":" + obj['country'] + "/" + obj['countryCode'] + "/" + flags.get(obj['countryCode']) + '\n' + "时区" + ":" + obj['timezone'] + '\n' + "城市" + ":" + obj['city'] + "/" + obj['regionName']+ '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
-$done({Title, Subtitle, ip, description});
+$done({title, subtitle, ip, description});
