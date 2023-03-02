cac840779446ad258deb1eaab668390a2068ad76
510004015 <105718141+510004015@users.noreply.github.com>
2023-03-02 22:54:18 +0800

    Update Cainiao.js

--- JavaScript/Cainiao.js
+++ JavaScript/Cainiao.js
@@ -1,5 +1,4 @@
 var modified = JSON.parse($response.body);
-modified.data.result.dataList[1].bizData.items.splice(0,3);
-modified.data.result.dataList[1].bizData.items.splice(1,1);
+modified.data.result.dataList[1].bizData.items.splice(0,4);
 modified.data.result.dataList.splice(2,1);
 $done({body:JSON.stringify(modified)});
