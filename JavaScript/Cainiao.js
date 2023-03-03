706e722a05443f5c3a741a202bcf126a1bad0301
510004015 <105718141+510004015@users.noreply.github.com>
2023-03-04 00:09:34 +0800

    Update Cainiao.js

--- JavaScript/Cainiao.js
+++ JavaScript/Cainiao.js
@@ -1,4 +1,3 @@
 var modified = JSON.parse($response.body);
-modified.data.result.dataList[1].bizData.items.splice(0,4);
-modified.data.result.dataList.splice(2,1);
+modified.data.result.dataList.splice(1,3);
 $done({body:JSON.stringify(modified)});
