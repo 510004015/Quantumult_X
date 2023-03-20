bddae1c5bd85476d2e1b9b66cdedd15e36c8c3bd
510004015 <105718141+510004015@users.noreply.github.com>
2023-03-21 00:34:30 +0800

    Update BuGuaKe.js

--- Premium/BuGuaKe.js
+++ Premium/BuGuaKe.js
@@ -2,7 +2,7 @@
 
 [rewrite_local]
 
-^https:\/\/appwk\.baidu\.com\/bgk\/(user\/getinfo|doc\/quedocsearch\?) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/BuGuaKe.js
+^https:\/\/appwk\.baidu\.com\/bgk\/(user\/getinfo|doc\/(getdocdownloadcopywriter|quedocsearch\?) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/BuGuaKe.js
 
 [mitm] 
 
@@ -14,6 +14,7 @@ var url = $request.url;
 var modified = JSON.parse($response.body);
 const URL1 = '/user/getinfo';
 const URL2 = '/doc/quedocsearch';
+const URL3 = '/doc/getdocdownloadcopywriter';
 if (url.indexOf(URL1) != -1) {
 modified.data.saveMoney.sumMoney = "510004015";
 modified.data.saveMoney.user_vip_tip = "永久会员";
@@ -22,5 +23,10 @@ modified.data.vip = {"jiaoyu_vip_info":{"uid":510004015,"is_vip":1,"start_time":
 };
 if (url.indexOf(URL2) != -1) {
 	var body = $response.body.replace(/"isLimit":\d/g, '"isLimit":0').replace(/"isPay":\d/g, '"isPay":0');
+};
+if (url.indexOf(URL3) != -1) {
+modified.data.downloadStatus = 19;
+modified.data.copywriter2 = "因为下载需要验证Token，So～想下载花钱吧。🤡";
+     body = JSON.stringify(modified);
 };
 $done({ body });
