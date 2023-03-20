1dea894b49b9c9a790bfc59f502beaf738c31f22
510004015 <105718141+510004015@users.noreply.github.com>
2023-03-20 23:08:06 +0800

    Create BuGuaKe.js

---
+++ Premium/BuGuaKe.js
@@ -0,0 +1,26 @@
+/*
+
+[rewrite_local]
+
+^https:\/\/appwk\.baidu\.com\/bgk\/(user\/getinfo|doc\/quedocsearch\?) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/BuGuaKe.js
+
+[mitm] 
+
+hostname = appwk.baidu.com
+
+*/
+
+var url = $request.url;
+var modified = JSON.parse($response.body);
+const URL1 = '/user/getinfo';
+const URL2 = '/doc/quedocsearch';
+if (url.indexOf(URL1) != -1) {
+modified.data.saveMoney.sumMoney = "510004015";
+modified.data.saveMoney.user_vip_tip = "永久会员";
+modified.data.vip = {"jiaoyu_vip_info":{"uid":510004015,"is_vip":1,"start_time":1672502400,"end_time":4070880000,"pro_total":510004015,"remain_day":510004015}};
+     body = JSON.stringify(modified);
+};
+if (url.indexOf(URL2) != -1) {
+	var body = $response.body.replace(/"isLimit":\d/g, '"isLimit":0').replace(/"isPay":\d/g, '"isPay":0');
+};
+$done({ body });
