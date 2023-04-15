4c278db61d009b0d23271e3dfe0ffa38f9b4d141
510004015 <105718141+510004015@users.noreply.github.com>
2023-04-15 21:55:26 +0800

    Update PikPak.js

--- Premium/PikPak.js
+++ Premium/PikPak.js
@@ -2,7 +2,7 @@
 
 [rewrite_local]
 
-^https:\/\/api-drive\.mypikpak\.com\/vip\/v\d\/vip\/info url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/PikPak.js
+^https:\/\/api-drive\.mypikpak\.com\/(vip\/v\d\/vip\/info|drive\/v\d\/about\?space) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/PikPak.js
 
 [mitm] 
 
@@ -10,9 +10,47 @@ hostname = api-drive.mypikpak.com
 
 */
 
-let Premium = $response.body;
-var modified = JSON.parse(Premium);
-modified.data.status = "ok",
-modified.data.expire = "2099-01-01T00:00:00+08:00",
-modified.data.type = "platinum";
+var url = $request.url;
+var modified = JSON.parse($response.body);
+const URL1 = '/vip/info';
+const URL2 = '/about';
+if (url.indexOf(URL1) != -1) {
+  modified = {
+  "data" : {
+    "expire" : "2024-01-01T00:00:00+08:00",
+    "status" : "ok",
+    "restricted" : {
+      "result" : false,
+      "content" : {
+        "color" : "",
+        "text" : "",
+        "deepLink" : ""
+      },
+      "learnMore" : {
+        "color" : "",
+        "text" : "",
+        "deepLink" : ""
+      }
+    },
+    "extUserInfo" : {
+      "userRegion" : "regional"
+    },
+    "user_id" : modified.data.user_id,
+    "type" : "platinum",
+    "fee_record" : "apple",
+    "vipItem" : [
+      {
+        "status" : "ok",
+        "expire" : "2024-01-01T00:00:00+08:00",
+        "type" : "regional",
+        "description" : "区域会员",
+        "surplus_day" : 365
+      }
+    ]
+  }
+}
+};
+if (url.indexOf(URL2) != -1) {
+  modified.quota.limit = "10995116277760";
+};
 $done({body:JSON.stringify(modified)});
