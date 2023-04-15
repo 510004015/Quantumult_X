b448aa6600d8c93db4fa040782e8e19550904d34
510004015 <105718141+510004015@users.noreply.github.com>
2023-04-15 22:10:00 +0800

    Update PikPak.js

--- Premium/PikPak.js
+++ Premium/PikPak.js
@@ -2,7 +2,7 @@
 
 [rewrite_local]
 
-^https:\/\/api-drive\.mypikpak\.com\/(vip\/v\d\/vip\/info|drive\/v\d\/about\?space) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/PikPak.js
+^https:\/\/api-drive\.mypikpak\.com\/(vip\/v\d\/(vip\/info|product\/productList)|drive\/v\d\/about\?space) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/PikPak.js
 
 [mitm] 
 
@@ -14,6 +14,7 @@ var url = $request.url;
 var modified = JSON.parse($response.body);
 const URL1 = '/vip/info';
 const URL2 = '/about';
+const URL3 = '/product/productList';
 if (url.indexOf(URL1) != -1) {
   modified = {
   "data" : {
@@ -52,5 +53,8 @@ if (url.indexOf(URL1) != -1) {
 };
 if (url.indexOf(URL2) != -1) {
   modified.quota.limit = "10995116277760";
+};
+if (url.indexOf(URL3) != -1) {
+  modified = {"platform":"apple","user_region":"","product_region":"regional","data":[{"product_id":"sub.month","promotional_id":"","intro_offer_id":"","promotional_offer_id":"","is_hot":false},{"product_id":"sub.year","promotional_id":"","intro_offer_id":"","promotional_offer_id":"","is_hot":true}],"description":null,"product_data":{"tab":null}};
 };
 $done({body:JSON.stringify(modified)});
