65115c282914a06b270ee9ec60b13a707e7d7573
510004015 <105718141+510004015@users.noreply.github.com>
2023-04-15 22:55:37 +0800

    Update PikPak.js

--- Premium/PikPak.js
+++ Premium/PikPak.js
@@ -2,7 +2,7 @@
 
 [rewrite_local]
 
-^https:\/\/api-drive\.mypikpak\.com\/(vip\/v\d\/vip\/info|drive\/v\d\/about\?space) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/PikPak.js
+^https:\/\/api-drive\.mypikpak\.com\/(vip\/v\d\/(vip\/info|allSubscriptionStatus)|drive\/v\d\/about\?space) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/PikPak.js
 
 [mitm] 
 
@@ -14,6 +14,7 @@ var url = $request.url;
 var modified = JSON.parse($response.body);
 const URL1 = '/vip/info';
 const URL2 = '/about';
+const URL3 = '/allSubscriptionStatus';
 if (url.indexOf(URL1) != -1) {
   modified.data.expire = "2099-01-01T00:00:00+08:00";
   modified.data.status = "ok";
@@ -30,5 +31,8 @@ if (url.indexOf(URL1) != -1) {
 };
 if (url.indexOf(URL2) != -1) {
   modified.quota.limit = "10995116277760";
+};
+if (url.indexOf(URL3) != -1) {
+  modified.quota.apple = {"subscribed":true,"purchased":true,"status":"","interval":"year","product":"sub.year","past_due_deadline":"","pay_type":"","region":"regional"};
 };
 $done({body:JSON.stringify(modified)});
