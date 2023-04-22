af1b9922a9f9c0d9bd439f21c82a6c4acadf44c5
510004015 <105718141+510004015@users.noreply.github.com>
2023-04-23 04:51:20 +0800

    Update Cainiao.js

--- JavaScript/Cainiao.js
+++ JavaScript/Cainiao.js
@@ -2,7 +2,7 @@ var url = $request.url;
 var body = JSON.parse($response.body);
 const URL1 = 'cainiao.guoguo.nbnetflow.ads.index.cn';
 const URL2 = 'cainiao.nbpresentation.protocol.homepage.get.cn';
-if (url.indexOf(URL1) != -1) {body.data.result.length=0};
+if (url.indexOf(URL1) != -1) {body.data.result.length=1};
 if (url.indexOf(URL2) != -1) {
 	body.data.result.dataList.pop();
 body.data.result.dataList[1].bizData.items.splice(0,3);
