249ba8a2f2f03280561d6f5e52b0e15f453b835d
510004015 <105718141+510004015@users.noreply.github.com>
2023-08-20 19:15:11 +0800

    Update 91porn.js

--- JavaScript/91porn.js
+++ JavaScript/91porn.js
@@ -1,2 +1,8 @@
-var body = $response.body.replace(/player\.preroll\([\s\S]+?\);/g, 'player.preroll();')
-$done({body})
+let reg = '<head>';
+let str = '<head>\
+<link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
+<script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let body = $response.body.replace(reg, str).replace(/player\.preroll\([\s\S]+?\);/g, 'player.preroll();');
+let headers = $response.headers;
+headers['Content-Security-Policy'] = '';
+$done({ headers, body, url })
